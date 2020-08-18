4a6c1dc8ca596712548b9f210d444efa7b1cbd41
Slaven Hrkac <shrkac@sh-mbp.local>
2020-08-18 10:01:58 +0200

    add command for installing all homebrew packages

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ ln -sv $orig/.mavenrc ~
 # Install brew 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# install brew packages
+brew bundle
+
 #git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
 
