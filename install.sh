4ee6911def0ec62cd6d3e9d3c9b501a5f88776bd
Slaven Hrkac <shrkac@gmail.com>
2020-08-21 15:35:05 +0200

    reorganize execution of commands

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 echo "Installing Mac Environment"
 orig=$(pwd)
-# Symlink most important config file
-ln -sv $orig/.zshrc ~
-ln -sv $orig/.sandboxrc ~
-ln -sv $orig/.mavenrc ~
 
 # Install brew 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -24,3 +20,8 @@ chsh -s "$(command -v zsh)"
 
 # install SDKMAN
 curl -s "https://get.sdkman.io" | bash
+
+# Symlink most important config file
+ln -sv $orig/.zshrc ~
+ln -sv $orig/.sandboxrc ~
+ln -sv $orig/.mavenrc ~
