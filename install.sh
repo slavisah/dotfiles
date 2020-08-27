d41114f885e4ca041c143978ebf2ab19e3a9100d
Slaven Hrkac <shrkac@gmail.com>
2020-08-27 18:27:20 +0200

    fix the aliases and add powerlevel10k to the installation script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ brew bundle
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
 
 # Set up shell
