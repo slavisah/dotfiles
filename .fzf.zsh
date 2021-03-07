f498b735c4167ddd05bfd7c6c9d9dd77a822abed
Slaven Hrkac <shrkac@gmail.com>
2021-03-07 19:57:41 +0100

    fix fzf path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */homebrew/opt/fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$HOME/homebrew/opt/fzf/shell/key-bindings.zsh"
 
